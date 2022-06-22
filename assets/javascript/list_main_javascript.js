465218628d37de1d29ee69280d90fb06202368ae
k689022123 <104609988+k689022123@users.noreply.github.com>
2022-06-22 21:56:00 +0800

    Update list_main_javascript.js

--- assets/javascript/list_main_javascript.js
+++ assets/javascript/list_main_javascript.js
@@ -33,34 +33,100 @@ var playlistClickFour = function () {
 
 playlistClickOne();
 
+var windowClientWidth = document.documentElement.clientWidth;
 var mainMusic = document.querySelector('.main__music');
 
 mainMusic.onscroll = function () {
-  if (mainMusic.scrollTop >= 0 && mainMusic.scrollTop < 884) {
-    playlist.forEach(function (element) {
-      element.classList.remove('active');
-    });
-    playlistOne.classList.add('active');
-  } else if (
-    mainMusic.scrollTop >= 884 &&
-    mainMusic.scrollTop < 2899.199951171875
-  ) {
-    playlist.forEach(function (element) {
-      element.classList.remove('active');
-    });
-    playlistTwo.classList.add('active');
-  } else if (
-    mainMusic.scrollTop >= 2899.199951171875 &&
-    mainMusic.scrollTop < 4914.39990234375
-  ) {
-    playlist.forEach(function (element) {
-      element.classList.remove('active');
-    });
-    playlistThree.classList.add('active');
-  } else {
-    playlist.forEach(function (element) {
-      element.classList.remove('active');
-    });
-    playlistFour.classList.add('active');
+  /*console.log(windowClientWidth);
+  console.log(mainMusic.scrollTop);*/
+
+  if (windowClientWidth <= 1536 && windowClientWidth > 1196) {
+    if (mainMusic.scrollTop >= 0 && mainMusic.scrollTop < 600) {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistOne.classList.add('active');
+    } else if (mainMusic.scrollTop >= 600 && mainMusic.scrollTop < 2060) {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistTwo.classList.add('active');
+    } else if (mainMusic.scrollTop >= 2060 && mainMusic.scrollTop < 3540) {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistThree.classList.add('active');
+    } else {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistFour.classList.add('active');
+    }
+  } else if (windowClientWidth == 1196) {
+    if (mainMusic.scrollTop >= 0 && mainMusic.scrollTop < 580) {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistOne.classList.add('active');
+    } else if (mainMusic.scrollTop >= 580 && mainMusic.scrollTop < 2060) {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistTwo.classList.add('active');
+    } else if (mainMusic.scrollTop >= 2060 && mainMusic.scrollTop < 3494) {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistThree.classList.add('active');
+    } else {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistFour.classList.add('active');
+    }
+  } else if (windowClientWidth <= 480 && windowClientWidth > 357) {
+    if (mainMusic.scrollTop >= 0 && mainMusic.scrollTop < 936) {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistOne.classList.add('active');
+    } else if (mainMusic.scrollTop >= 936 && mainMusic.scrollTop < 3225) {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistTwo.classList.add('active');
+    } else if (mainMusic.scrollTop >= 3225 && mainMusic.scrollTop < 5514) {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistThree.classList.add('active');
+    } else {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistFour.classList.add('active');
+    }
+  } else if (windowClientWidth <= 357) {
+    if (mainMusic.scrollTop >= 0 && mainMusic.scrollTop < 1838) {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistOne.classList.add('active');
+    } else if (mainMusic.scrollTop >= 1838 && mainMusic.scrollTop < 6379) {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistTwo.classList.add('active');
+    } else if (mainMusic.scrollTop >= 6379 && mainMusic.scrollTop < 10920) {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistThree.classList.add('active');
+    } else {
+      playlist.forEach(function (element) {
+        element.classList.remove('active');
+      });
+      playlistFour.classList.add('active');
+    }
   }
 };
